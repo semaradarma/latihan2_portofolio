b69e355a25dd06d5da381dcac76edc10847b09fc
semaradarma <darmaiputusemara.com>
2024-09-28 12:47:13 +0800

    saya sudah update

---
+++ website_portfolio/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html',
+  ],
+  theme: {
+    extend: {
+      letterSpacing: {
+        ngempit:'-0.5em',
+        ngangkang: '.5em',
+      }
+    },
+  },
+  plugins: [],
+  
+}
+
